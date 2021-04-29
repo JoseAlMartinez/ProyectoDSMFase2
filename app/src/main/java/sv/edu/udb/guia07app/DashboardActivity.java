6e35f4eadee4e30f262d54a629300959842d5e06
Machuca22 <jairo.mp.99@gmail.com>
2021-04-29 11:30:54 -0600

    Creacion de pantallas del dash
    Creacion de  3 pantallas del dash con respecto a las categorias

--- app/src/main/java/sv/edu/udb/guia07app/DashboardActivity.java
+++ app/src/main/java/sv/edu/udb/guia07app/DashboardActivity.java
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -24,6 +25,7 @@ public class DashboardActivity extends AppCompatActivity {
     ActionBarDrawerToggle actionBarDrawerToggle;
     NavigationView navigationView;
     Button btn_shop;
+    ImageView ima1,ima2,ima3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,11 +74,43 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
 
+
+        //Ingreso al menu de hamburgesas
+        ima1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, HamburguesasActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Ingreso al menu de pizzas
+        ima2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, PizzaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Ingreso al menu de Postres
+        ima3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, PostresActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     private void initializeViews() {
         btn_shop = findViewById(R.id.btn_shop);
+        ima1 = findViewById(R.id.hamburger_img);
+        ima2 = findViewById(R.id.pizzasView1);
+        ima3 = findViewById(R.id.postresView);
     }
+
 
 
     public void setUpToolbar() {
