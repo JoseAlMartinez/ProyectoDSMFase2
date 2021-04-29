f44837e4c93059aee639a35596057f821938cbfb
cmar01 <reyalexmendoza@gmail.com>
2021-04-29 17:04:20 -0600

    Vista para productos

--- app/src/main/java/sv/edu/udb/guia07app/PostresActivity.java
+++ app/src/main/java/sv/edu/udb/guia07app/PostresActivity.java
@@ -7,12 +7,42 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import sv.edu.udb.guia07app.Modelo.Producto;
+import sv.edu.udb.guia07app.Productos.ActividadProducto;
+import sv.edu.udb.guia07app.Productos.AdaptadorProducto;
+import sv.edu.udb.guia07app.Productos.AgregarProducto;
+import sv.edu.udb.guia07app.R;
 
 public class PostresActivity extends AppCompatActivity {
 
-    private TextView mTextViewData;
-    private DatabaseReference mDatabase;
+    public static FirebaseDatabase database = FirebaseDatabase.getInstance();
+    public static DatabaseReference refProductos = database.getReference("productos");
 
+    // Ordenamiento
+    Query consultaOrdenada = refProductos.orderByChild("categoria").equalTo("Postres");
+
+    List<Producto> productos;
+    ListView listaProductos;
 
     //Declarando botones de navegacion
     Button btn_shop;
@@ -21,11 +51,45 @@ public class PostresActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_postres);
 
-
+        inicializar();
 
     }
 
+    private void inicializar() {
+        FloatingActionButton fab_agregar= findViewById(R.id.btn_agregar);
+        listaProductos = findViewById(R.id.ListaPostres);
+
+        // Cuando el usuario haga clic en la lista (para editar registro)
 
 
+        // Cuando el usuario hace un LongClic (clic sin soltar elemento por mas de 2 segundos)
+        // Es por que el usuario quiere eliminar el registro
+
+        productos = new ArrayList<>();
+
+        // Cambiarlo refProductos a consultaOrdenada para ordenar lista
+        consultaOrdenada.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Procedimiento que se ejecuta cuando hubo algun cambio
+                // en la base de datos
+                // Se actualiza la coleccion de personas
+                productos.removeAll(productos);
+                for (DataSnapshot dato : dataSnapshot.getChildren()) {
+                    Producto producto = dato.getValue(Producto.class);
+                    producto.setKey(dato.getKey());
+                    productos.add(producto);
+                }
+
+                AdaptadorVista adapter = new AdaptadorVista(PostresActivity.this,
+                        productos);
+                listaProductos.setAdapter(adapter);
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
 
 }
